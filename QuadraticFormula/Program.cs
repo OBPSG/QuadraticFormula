cfab66c2d8b2313bb630302a8e5bc3549ff1b12c
OBPSG <vbeasupergamer@gmail.com>
2021-03-02 12:45:29 -0600

    Initial Commit

---
+++ QuadraticFormula/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuadraticFormula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
